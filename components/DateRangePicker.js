53abb6805ce82e15f5e55eb4c2941d87f3dd6be2
Flavio Copes <copesc@gmail.com>
2019-11-13 16:50:50 +0100

    Sync start and end dates

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -16,9 +16,26 @@ const formatDate = (date, format, locale) =>
 
 const format = 'dd MMM yyyy'
 
+const today = new Date()
+const tomorrow = new Date(today)
+tomorrow.setDate(tomorrow.getDate() + 1)
+
+const numberOfNightsBetweenDates = (startDate, endDate) => {
+  const start = new Date(startDate) //clone
+  const end = new Date(endDate) //clone
+  let dayCount = 0
+
+  while (end > start) {
+    dayCount++
+    start.setDate(start.getDate() + 1)
+  }
+
+  return dayCount
+}
+
 export default () => {
-  const [startDate, setStartDate] = useState(new Date())
-  const [endDate, setEndDate] = useState(new Date())
+  const [startDate, setStartDate] = useState(today)
+  const [endDate, setEndDate] = useState(tomorrow)
 
   return (
     <div className='date-range-picker-container'>
@@ -27,6 +44,7 @@ export default () => {
         <DayPickerInput
           formatDate={formatDate}
           format={format}
+          value={startDate}
           parseDate={parseDate}
           placeholder={`${dateFnsFormat(new Date(), format)}`}
           dayPickerProps={{
@@ -38,6 +56,11 @@ export default () => {
           }}
           onDayChange={day => {
             setStartDate(day)
+            if (numberOfNightsBetweenDates(day, endDate) < 1) {
+              const newEndDate = new Date(day)
+              newEndDate.setDate(newEndDate.getDate() + 1)
+              setEndDate(newEndDate)
+            }
           }}
         />
       </div>
@@ -46,13 +69,17 @@ export default () => {
         <DayPickerInput
           formatDate={formatDate}
           format={format}
+          value={endDate}
           parseDate={parseDate}
           placeholder={`${dateFnsFormat(new Date(), format)}`}
           dayPickerProps={{
             modifiers: {
-              disabled: {
-                before: new Date()
-              }
+              disabled: [
+                startDate,
+                {
+                  before: startDate
+                }
+              ]
             }
           }}
           onDayChange={day => {
