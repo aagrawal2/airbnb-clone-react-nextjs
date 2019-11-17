9b856e81029ec47b57009ea272633045236a76ff
Flavio Copes <copesc@gmail.com>
2019-11-17 15:30:04 +0100

    Implement model and database connection

---
+++ database.js
@@ -0,0 +1,4 @@
+export const user = 'flaviocopes'
+export const password = ''
+export const host = 'localhost'
+export const database = 'nextbnb'
--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,8 +1,29 @@
-export default (req, res) => {
+import { User } from '../../../model.js'
+
+export default async (req, res) => {
   if (req.method !== 'POST') {
     res.status(405).end() //Method Not Allowed
     return
   }
-  console.log(req.body)
-  res.end()
+
+  const { email, password, passwordconfirmation } = req.body
+
+  if (password !== passwordconfirmation) {
+    res.end(
+      JSON.stringify({ status: 'error', message: 'Passwords do not match' })
+    )
+    return
+  }
+
+  try {
+    const user = await User.create({ email, password })
+    res.end(JSON.stringify({ status: 'success', message: 'User added' }))
+  } catch (error) {
+    res.statusCode = 500
+    let message = 'An error occurred'
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      message = 'User already exists'
+    }
+    res.end(JSON.stringify({ status: 'error', message }))
+  }
 }
